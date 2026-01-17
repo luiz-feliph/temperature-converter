d0e6a096eed28ea8f48608e233d1977e11868fb2
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-01-16 22:43:21 -0300

    Main test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,9 +1,15 @@
 public class Test {
 
     public static void main(String[] args) {
-
+        if (runTest()) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
     }
 
-    void runTest() {}
+    public static boolean runTest() {
+        return true;
+    }
 
 }
