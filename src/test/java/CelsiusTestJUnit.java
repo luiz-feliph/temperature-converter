b3d5d7b5acfa7ecaf9838c2c25e7b8fe085601e9
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-01-18 19:16:15 -0300

    Changing functions name

--- src/test/java/CelsiusTestJUnit.java
+++ src/test/java/CelsiusTestJUnit.java
@@ -8,22 +8,22 @@ public class CelsiusTestJUnit {
     private final Celsius celsius = new Celsius();
 
     @Test
-    void _30_celsius_is_86_fahrenheit() {
+    public void _30_celsius_is_86_fahrenheit() {
         assertEquals(86, celsius.toFahrenheit(30));
     }
 
     @Test
-    void _20_celsius_is_68_fahrenheit() {
+    public void _20_celsius_is_68_fahrenheit() {
         assertEquals(68, celsius.toFahrenheit(20));
     }
 
     @Test
-    void _10_celsius_is_50_fahrenheit() {
+    public void _10_celsius_is_50_fahrenheit() {
         assertEquals(50, celsius.toFahrenheit(10));
     }
 
     @Test
-    void _minus_274_celsius_is_impossible() {
+    public void negative_274_celsius_is_not_defined() {
         assertThrows(RuntimeException.class, () -> celsius.toFahrenheit(-274));
     }
 }
--- src/test/java/FahrenheitTestJUnit.java
+++ src/test/java/FahrenheitTestJUnit.java
@@ -8,37 +8,37 @@ public class FahrenheitTestJUnit {
     private final Fahrenheit fahrenheit = new Fahrenheit();
 
     @Test
-    void _0_fahrenheit_is_minus_17_celsius() {
+    public void _0_fahrenheit_is_minus_17_celsius() {
         assertEquals(-17, fahrenheit.toCelsius(0));
     }
 
     @Test
-    void _10_fahrenheit_is_minus_12_celsius() {
+    public void _10_fahrenheit_is_minus_12_celsius() {
         assertEquals(-12, fahrenheit.toCelsius(10));
     }
 
     @Test
-    void _20_fahrenheit_is_minus_6_celsius() {
+    public void _20_fahrenheit_is_minus_6_celsius() {
         assertEquals(-6, fahrenheit.toCelsius(20));
     }
 
     @Test
-    void _30_fahrenheit_is_minus_1_celsius() {
+    public void _30_fahrenheit_is_minus_1_celsius() {
         assertEquals(-1, fahrenheit.toCelsius(30));
     }
 
     @Test
-    void _32_fahrenheit_is_0_celsius() {
+    public void _32_fahrenheit_is_0_celsius() {
         assertEquals(0, fahrenheit.toCelsius(32));
     }
 
     @Test
-    void _86_fahrenheit_is_30_celsius() {
+    public void _86_fahrenheit_is_30_celsius() {
         assertEquals(30, fahrenheit.toCelsius(86));
     }
 
     @Test
-    void _460_fahrenheit_is_impossible() {
+    public void negative_460_fahrenheit_is_not_defined() {
         assertThrows(RuntimeException.class, () -> fahrenheit.toCelsius(-460));
     }
 }
